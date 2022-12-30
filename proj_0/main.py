a6959fd885686df3c72cc92346dc4f0d08fc4bac
Zacharias Böhler <zacharias.boehler@gmail.com>
2022-12-30 16:40:08 +0100

    Add files via upload

---
+++ proj_0/main.py
@@ -0,0 +1,304 @@
+from revChatGPT.ChatGPT import Chatbot
+
+chatbot = Chatbot({
+  "session_token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..i0UGJkKEzlNtGCe6.ttOFl-cVvmfoNbclaX-qKako_2w7qVClGPJwD-yVtfSSj-zOmVkDQ81Fr9Ka4_4YNKe5rsQ1n6isnXGfRYPa4wC14qG7s2cpOKP62503bm_-NhyLx8gCHDhXR-WG93p_UNUp2vJph0ibG_1FDB7TGyvZJQmJ_6dAxzMcWgxJBCjU_VhgJVOOHulpuyD-tgWtsnbQrLFW5J6CGYsk_ai6aOBjMRkaNCM46nfkvVp10mZLyQm8uKRWzkiyAP3-JzHd8yqXUSCe1WdYihC5LGAzURIj8mir2N89X2c1jxt3KpOb5a1VUwnqUdyEaE51_H2PNWGcUzX-8IGZXvYBljHEBnGZVCelmbHkD1XHwM1KrQDEMoqGq3oLWl9xoQ-RX79LkpsKeBQyNr6dSch6aR4SITHZ_O5wFRpja6nmC7kiNrzIa3tx49c1rsvAoCJl_5Q61QJUEhqSBew82oeamaKj94ahMKzFsWIPO2xOZmZCVRJw2OFOySxQ-YelQiMkTk3MCLfo5i4FR5rxIsfwkoPCQSRQ1-x6SbAjdPu1SGFAJxJZCGQhCZDssdWwHt-jiJIMDr_3kTMUqf-kHZsFCeP99bl_oZF3SzOkDC1_A6RDGxCEjJkzdBuNqsJ4WwQ0eN-erQNMIK86HjjxlWJP1eXXFxlhwx40yFKPX764sDcp7h17JXmgLkn2uwQCJvdsGJhRibH3OxZaAGXpkvleDwjs0XxozHh5sqodI4UJB1aT-yyCrd4TnBGE9W9sp9mfkt4Nlq1xqRbzQbDb9LXPbB2xce8t4_d2ZXOEBb0odaWhFcCuJC0EW_7QeI--JpFgv9TfgV2Jgr4fOzWBvoRiIoxcuyOEbzM7s_RKGE7167yYQXwZeV5fVflZv10WRkpQHiwZOmcoSgjBQITcih5N9LY6ltlUXESRxttnu-CjpFl_EMhbGYNjQrcsZS83DrEjwYa6FlYQ2guMemxgCzKqHNgX8d_O9QrEAfJmO805CCzMHkdrmuPKio_EyPWNWYRJcCShgzelxRH6bZWriQIOFPZ12SUPtcO-Xjqe05QNa9jWlLwapmZkOG9-7NEo9DZ_YektFJZNUj0o3jWyqOrMPENJpD-yVwMzEitPKi4QDMedvCwD_nCsUjhqsCiQofK_D5P4T6DXDs9aq9PdjL6wW6keUo9e5rEvkHl2lmn54lV7U-6DoexfnDS0e_d7p7VuRjeAIFxEL3DGgdMHTZTTuWbl8Ka2WkonYHFPXTk4wmiBv0ScWSt_fM1BB0OmNNL9vzGqJndA2BhlbBuyzT4L3gYtj2rynqyB1wSghompJizmmfBng6QGPEqUCsVovuGq73kGQ_-tLfXfSlidl_fKnoftUGFaPbS3Ibf3EaQHZbO6ph_fYJxW1H9xj8INBFUTIlAyEHaOL7cPtZfGHkLW8Cg3mxDgmxfRtCHbDKz8jgLIMN0zAxqnJcL1L3XAGko7FR8HCq_vXPe_0Ju-5Y62o4ZXNIJ05mBZDAcNyY0mohLYENqg_4ez7O1wBn5N3MbsCQ7OXSv5VtvK2vdf4ETWzEv4WeYovv75gvXgcyZ1qOx4-CDgjidntrhllFwRsW_fFe5AnjPhsRuX4q7XGKYhyht3H2HilEGXEz5Yx29NgDj4kU2siVVcwwFkKBg9LcbB4LGaXF375e1HXqnq_2CdNKdhshQP9XDwlGOR3YjOU4AzGxT1RqCqyEQuWLa1ghrnBxJrUgysdNgg949sDAdUvupC8bfPHnuy1kEny9TTMQOiaeTuwTlaBgdlZbgIZeSYaKvrHnAeVdJ_wvvUjYo2OHCdhbrFR1PNVm0vz1gJXYEFaVtni6-7l3ACTHOazpMG1-WD-ogAWb3jOuXftT3HWfzO2XMRr-rc0NYxVmuxs91jiHn1V0HtuEzrL0FGf3GG-SBz_Hfq3zGDg5RRW25sH8_i5LxYUuQbMLX12k7MeiJzXtZ6fIMu06lMetSPIjLNxmWuk07Ykp4bl3oPn79BTHhpDuTMZBZpGFYKWw47qdpU2EJnl8tEZIKQajPGXQ2DfL-lGcYiEp1EIcratL7UC7exFJ04s3jJ6qsiix4jBhNgRL53f3DPTpvuxuPJcnYcqY8JqqAn_0xCjdocSppQizb3xaswsLaR969Zk46FU__-sBRJzwFH5BoB5IpgmO32h3q_lDK-pFw2LjwA8Z0u4I3kiTOlM1-WWCfZ4xapEO-ozcQNJaPTFQmwBz6VorSPb4TQSCbNpWZIeUYE2C4B4UNA12WU2NG6APUCo0FFWObwxwacHzORUja_kHgI7RdyKvCpMAK-SkfSVw3xHW5HfbQ4IWDaAx1D3rtTXaM48S9M1pswuIo2s_l1uiwCog6K-xq6x3lMQW1uYUYuEd7AKJ3KmJZ5Ic4lmsHjorRibBSjyxzqJ5EEHw.CKzK-_DwYu7hWf2wtaThsQ"
+}, conversation_id=None, parent_id=None) # You can start a custom conversation
+#
+#
+"""
+while True:
+  prompt = input ("Prompt: ")
+
+
+  response = chatbot.ask("answer with just a single json object containing information about " + prompt+":", conversation_id=None, parent_id=None) # You can specify custom conversation and parent ids. Otherwise it uses the saved conversation (yes. conversations are automatically saved)
+  print (f"\n\n{response['message']}\n")
+"""
+
+raw_html_home = """
+<html>
+<head></head>
+<body>
+
+  <input type="text" id="search" / >
+  <button onclick="window.location.href = './json/' + encodeURIComponent(document.getElementById('search').value);" >Search</button>
+</body>
+</html>
+"""
+
+# https://raw.githubusercontent.com/moappi/visualizer.json2html/master/lib/visualizer.js
+# https://raw.githubusercontent.com/moappi/visualizer.json2html/master/css/visualizer.css
+raw_html_json = """
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/json2html/1.3.0/json2html.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.json2html/1.2.0/jquery.json2html.min.js"></script>
+
+    <!-- Visualizer Core -->
+    <style>
+    .visual-package {padding:3px;border-radius:2px;margin-top:3px;}
+.visual-header {cursor:pointer;}
+
+.visual-name {color:gray;}
+
+.visual-array {background-color:#FFD8BB;border:thin solid #FFB780;}
+.visual-object {background-color:#E7F1FE;border:thin solid #7DA2CE;}
+.visual-string {color:red;}
+.visual-number {color:blue;}
+.visual-function {color:green;}
+
+.visual-open .visual-children {display:block;}
+.visual-closed .visual-children {display:none;}
+
+.visual-arrow {background-image:url("https://github.com/moappi/visualizer.json2html/raw/master/css/d.png"); background-repeat:no-repeat; background-color:transparent; height:15px; width:15px; display:inline-block;}
+
+.visual-open .visual-arrow {background-position:-20px 0;}
+.visual-closed .visual-arrow {background-position:0 0;}
+
+.visual-type {color:gray;font-size:8pt;float:right;}
+
+.hide {
+    display:none !important;
+}
+    </style>
+    <script type="text/javascript">//     visualizer.json2html.js
+//     https://www.json2html.com
+//     (c) 2006-2020 Crystalline Technologies
+//     json2html may be freely distributed under the MIT license.
+
+function visualizer($node) {
+        
+    var base = this;
+    
+    //Create a new visualizer attached to the result tab
+    base.node = $node;
+    
+    //Transform used to convert into HTML
+    base.transform = {"<>":"div","class":"visual-package visual-${show} visual-${type}","html":[
+        {"<>":"div","class":"visual-header","html":[
+          {"<>":"div","class":function(obj){
+    
+            var classes = ["visual-arrow"];
+    
+            if( base.getValue(obj.value) !== undefined ) classes.push("hide");
+            
+            return(classes.join(" "));
+          }},
+          {"<>":"span","class":"visual-name","text":"${name}"},
+          {"<>":"span","class":"visual-value","text":function(obj) {
+            var value = base.getValue(obj.value);
+            if( value !== undefined ) return(" : " + value);
+            else return("");
+          }},
+          {"<>":"span","class":"visual-type","text":"${type}"}
+        ]},
+        {"<>":"div","class":"visual-children","html":function(obj){return(base.children(obj.value));}}
+      ]};
+}
+
+visualizer.prototype = {
+    
+    "visualize":function(json){
+        
+        var base = this;
+        
+        //Convert json into html 
+        base.node.empty().json2html(base.convert("json",json,"open"),base.transform);
+        
+        //Set the events
+        base.events();
+    },
+    
+    //Get a specific value from the json object
+    "getValue":function(obj) {
+      var type = $.type(obj);
+    
+      //Determine if this object has children
+      switch(type) {
+        case "array":
+        case "object":
+          return(undefined);
+        break;
+    
+        case "function":
+          //none
+          return("function");
+        break;
+    
+        case "string":
+          return("'" + obj + "'");
+        break;
+    
+        default:
+          return(obj);
+        break;
+      }
+    },
+    
+    //Transform the children
+    "children":function(obj) {
+        
+        var base = this;
+        
+      var type = $.type(obj);
+    
+      //Determine if this object has children
+      switch(type) {
+        case "array":
+        case "object":
+          return(json2html.transform(obj,base.transform));
+        break;
+    
+        default:
+          //This must be a litteral
+        break;
+      }
+    },
+    
+    //Convert this json object to HTML
+    "convert":function(name,obj,show) {
+        
+        var base = this;
+      
+      var type = $.type(obj);
+    
+      if(show === undefined) show = "closed";
+      
+      var children = [];
+    
+      //Determine the type of this object
+      switch(type) {
+        case "array":
+          //Transform array
+          //Itterrate through the array and add it to the elements array
+          var len=obj.length;
+          for(var j=0;j<len;++j){ 
+            //Concat the return elements from this objects tranformation
+            children[j] = base.convert(j,obj[j]);
+          }
+        break;
+    
+        case "object":
+          //Transform Object
+          var j = 0;
+          for(var prop in obj) {
+            children[j] = base.convert(prop,obj[prop]);
+            j++;
+          } 
+        break;
+    
+        default:
+          //This must be a litteral (or function)
+          children = obj;
+        break;
+      }
+    
+      return( {"name":name,"value":children,"type":type,"show":show} );
+      
+    },
+    
+    //Set the events
+    "events":function(){
+        
+        var base = this;
+        
+        //Set the header click event
+        base.node.find('.visual-header').click(function(){
+        
+        var $parent = $(this).parent();
+            
+            //Toggle the closed element
+            if($parent.hasClass("visual-closed")) {
+              $parent.removeClass("visual-closed");
+              $parent.addClass("visual-open");
+            } else {
+              $parent.removeClass("visual-open");
+              $parent.addClass("visual-closed");
+            } 
+        });
+    }
+}</script>
+
+</head>
+
+<body>
+    <div id="output"></div>
+</body>
+
+<script>
+    
+    var json = [JSON_DATA];
+    
+    $(function(){
+        
+      //Create a new visualizer object
+      var _visualizer = new visualizer($("#output"));
+      
+      //Visualize the demo json object
+      _visualizer.visualize(json);
+    
+    });
+</script>
+
+</html>"""
+
+
+
+import falcon
+
+class Home(object):
+    def on_get(self,req,resp):
+        resp.status = falcon.HTTP_200
+        resp.set_header("Access-Control-Allow-Origin",'*')
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+        resp.set_header('Content-Type', 'text/html')
+
+        resp.body = raw_html_home
+        resp.status_code = falcon.HTTP_200
+        return
+
+
+class Json(object):
+    def on_get(self,req,resp,prompt):
+        resp.status = falcon.HTTP_200
+        resp.set_header("Access-Control-Allow-Origin",'*')
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+        resp.set_header('Content-Type', 'text/html')
+
+        print (prompt)
+
+        mod_prompt = chatbot.ask("answer with just a single json object containing highly detailed information about " + prompt+":", conversation_id=None, parent_id=None)['message']
+
+        filtered_prompt = mod_prompt[mod_prompt.find("{"):mod_prompt.rfind("}")+1]
+
+        resp.body = raw_html_json.replace("[JSON_DATA]",filtered_prompt)
+        resp.status_code = falcon.HTTP_200
+        return
+
+
+
+App = falcon.App()
+
+# Routes
+home = Home()
+_json = Json()
+
+App.add_route('/',home)
+App.add_route('/json/{prompt}',_json)
+
+from waitress import serve
+
+serve(App, host="0.0.0.0", port=80)
+
+
+
+
+
